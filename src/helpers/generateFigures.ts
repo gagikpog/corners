201d5aea2676415011fdf0b13684b9197cd4a11a
Gagik <gagikpog@gmail.com>
2024-06-04 18:10:36 +0500

    change figures count

--- src/helpers/generateFigures.ts
+++ src/helpers/generateFigures.ts
@@ -6,15 +6,15 @@ export function generateFigures(color: Color): IFigure[] {
 
     const figures: IFigure[] = [];
 
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            figures.push(new Figure({x: i, y: j, color: Color.Black, owner: color === Color.Black, id: getFigureId()}));
+    for (let y = 0; y < 3; y++) {
+        for (let x = 0; x < 4; x++) {
+            figures.push(new Figure({x, y, color: Color.Black, owner: color === Color.Black, id: getFigureId()}));
         }
     }
 
-    for (let i = 5; i < 8; i++) {
-        for (let j = 5; j < 8; j++) {
-            figures.push(new Figure({x: i, y: j, color: Color.White, owner: color === Color.White, id: getFigureId()}));
+    for (let y = 5; y < 8; y++) {
+        for (let x = 4; x < 8; x++) {
+            figures.push(new Figure({x, y, color: Color.White, owner: color === Color.White, id: getFigureId()}));
         }
     }
 
